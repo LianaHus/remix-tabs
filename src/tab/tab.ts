2f7b2df5f3df15e8f0b169cc5b261dc4ad3ef78f
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-02-07 13:23:56 +0100

    Add style into static property

--- src/tab/tab.ts
+++ src/tab/tab.ts
@@ -1,9 +1,32 @@
-import { customElement, LitElement, property, TemplateResult, html } from "lit-element";
+import { customElement, LitElement, property, TemplateResult, html, css } from "lit-element";
 import { Tab } from './model';
 
 @customElement('remix-tab')
 export class RemixTab extends LitElement {
   
+  static styles = [
+    css`:host {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      background: #dee1e6;
+      border-radius: 6px 6px 0px 0px;
+      padding: 5px 10px;
+    }`,
+    css`.title {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      font-family: Arial;
+      font-size: 14px;
+    }`,
+    css`.close {
+      padding-left: 5px;
+      cursor: pointer;
+    }`,
+  ];
+
   @property({ type: Object })
   public tab: Tab;
   
@@ -30,54 +53,11 @@ export class RemixTab extends LitElement {
    */
   render(): TemplateResult {
     return html`
-
-    <style>
-    
-    .close {
-      float:right;
-      display:inline-block;
-      padding:0px 4px;
-      background:#dee1e6; 
-      font-stretch: expanded;
-    }
-    
-    .close:hover {
-      float:right;
-      display:inline-block;
-      padding:0px 4px;
-      background:#dee1e6;
-      color:#fff;
-      cursor: pointer;
-      font-stretch: expanded;
-    }
-    
-    .tab {
-      width: 80px;
-        font-family: Arial;
-        font-size: 14px;
-        height: 18px;
-        padding: 8px 4px 4px 8px;
-        background: #dee1e6;
-        border-radius: 6px 6px 0px 0px;
-        border-color: red;
-        overflow: hidden;
-        display: inline-block;
-        text-overflow: ellipsis;
-        margin-left: 0px;
-        margin-right: 0px;
-    }
-    
-    .active-tab tab {
-      background: #ddd;
-    }
-    
-    </style>
-
-    <div class='tab' title="${this.tab.tooltip}" >
+    <div class="title" title="${this.tab.tooltip}" >
       <img src="${this.tab.icon}" />
       <span>${this.tab.title}</span>
-      <span class='close' @click="${this.closeTab}">x</span>
     </div>
+    <span class="close" @click="${this.closeTab}">X</span>
   `;
   }
 }
--- src/tab/tabs.ts
+++ src/tab/tabs.ts
@@ -1,18 +1,42 @@
-import { customElement, LitElement, property, TemplateResult, html } from "lit-element";
+import { customElement, LitElement, property, TemplateResult, html, css } from "lit-element";
 import { Tab, defaultTab } from './model';
 
 @customElement('remix-tabs')
 export class RemixTabs extends LitElement {
 
+  static styles = [
+    css`:host {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }`,
+    css`button {
+      margin-left: 10px;
+      height: 25px;
+      width: 25px;
+      border-radius: 50%;
+      border: none;
+      cursor: pointer;
+      box-shadow: 0 0 2px rgb(50, 50, 50);
+    }`,
+    css`button:hover {
+      box-shadow: 0 0 3px rgb(50, 50, 50);
+    }`
+  ];
+
   @property({ type: Array, reflect: true })
   public tabs: Partial<Tab>[] = [];
 
-  /** Add a tab locally */
+  @property({ type: Array, reflect: true })
+  public activated: string;
+
+  /** Add a tab to the list */
   public addTab() {
     const tab = defaultTab(this.tabs.length);
     this.tabs = [ ...this.tabs, tab ];
   }
 
+  /** Remove a specific tab from the list */
   public removeTab({ detail: id }: CustomEvent) {
     const index = this.tabs.indexOf(id);
     this.tabs = [...this.tabs.slice(0, index), ...this.tabs.slice(index, this.tabs.length - 1)];
@@ -24,11 +48,8 @@ export class RemixTabs extends LitElement {
       return html`<remix-tab tab='${JSON.stringify(tab)}' @tabClosed=${e => this.removeTab(e)}></remix-tab`;
     });
     return html`
-    <div class='tab-container'>
-      <label>${this.tabs.length}</label>
       ${remixTabs}
       <button @click="${this.addTab}">+</button>
-    </div>
     `;
   }
 }
